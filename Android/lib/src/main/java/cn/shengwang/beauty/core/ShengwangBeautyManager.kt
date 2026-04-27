0019a68aced8846da938a8acd9b02b55c793ec37
zoucheng <zoucheng@agora.io>
2026-04-27 16:32:44 +0800

    [FIX] add inner log switch

--- Android/lib/src/main/java/cn/shengwang/beauty/core/ShengwangBeautyManager.kt
+++ Android/lib/src/main/java/cn/shengwang/beauty/core/ShengwangBeautyManager.kt
@@ -5,6 +5,10 @@ import io.agora.rtc2.Constants
 import io.agora.rtc2.IVideoEffectObject
 import io.agora.rtc2.RtcEngine
 import io.agora.rtc2.video.FaceShapeAreaOptions
+import org.json.JSONException
+import org.json.JSONObject
+
+
 
 /**
  * 声网美颜管理器
@@ -76,6 +80,9 @@ object ShengwangBeautyManager {
             return false
         }
 
+        // only in debug mode
+//        enableInnerLog(true)
+
         // Enable beauty by default
         enable(true)
 
@@ -138,6 +145,21 @@ object ShengwangBeautyManager {
             enableFilter(false)
             enableMakeup(false)
             enableSticker(false)
+        }
+    }
+
+    private fun enableInnerLog(enable: Boolean) {
+        val debugObj = JSONObject()
+        try {
+            debugObj.put("logfile_enable", enable)
+            if (rtcEngine != null) {
+                rtcEngine?.setExtensionProperty(
+                    "agora_video_filters_clear_vision", "clear_vision",
+                    "debug_param", debugObj.toString()
+                )
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
         }
     }
 
